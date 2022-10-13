4ca0ae77b0ac2735ae1010d14afa92cf10f8ea86
fewrie <sajsandhu10@gmail.com>
2022-10-13 03:04:59 -0400

    Userflow from login to pet dashboard added +image resources +icons etc

--- app/src/main/java/com/wekomodo/onlinetails/LoginActivity.kt
+++ app/src/main/java/com/wekomodo/onlinetails/LoginActivity.kt
@@ -1,11 +1,26 @@
 package com.wekomodo.onlinetails
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.wekomodo.onlinetails.databinding.ActivityDashboardBinding
+import com.wekomodo.onlinetails.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.loginButton.setOnClickListener{
+            val intent = Intent(this,DashboardActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        binding.signupButton.setOnClickListener{
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
